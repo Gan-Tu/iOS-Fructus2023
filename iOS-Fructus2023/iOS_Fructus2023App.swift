95d86532d59b5a0541f7f8f2ccd6906fb117ec37
Gan Tu <github@tugan.dev>
2023-07-16 17:36:20 -0400

    Initial Commit

---
+++ iOS-Fructus2023/iOS_Fructus2023App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Fructus2023App.swift
+//  iOS-Fructus2023
+//
+//  Created by Gan Tu on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Fructus2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
