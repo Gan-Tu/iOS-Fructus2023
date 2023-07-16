15868e48508b45ed83ebe934aa223c02690844c6
Gan Tu <github@tugan.dev>
2023-07-16 17:50:39 -0400

    feat: initial skeleton

---
+++ iOS-Fructus2023/Utilities/ColorExtension.swift
@@ -0,0 +1,39 @@
+//
+//  ColorExtension.swift
+//  iOS-Fructus2023
+//
+//  Created by Gan Tu on 7/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let apple_dark = Color("ColorAppleDark")
+    static let apple_light = Color("ColorAppleLight")
+    static let blueberry_dark = Color("ColorBlueberryDark")
+    static let blueberry_light = Color("ColorBlueberryLight")
+    static let cherry_dark = Color("ColorCherryDark")
+    static let cherry_light = Color("ColorCherryLight")
+    static let gooseberry_dark = Color("ColorGooseberryDark")
+    static let gooseberry_light = Color("ColorGooseberryLight")
+    static let grapefruit_dark = Color("ColorGrapefruitDark")
+    static let grapefruit_light = Color("ColorGrapefruitLight")
+    static let lemon_dark = Color("ColorLemonDark")
+    static let lemon_light = Color("ColorLemonLight")
+    static let lime_dark = Color("ColorLimeDark")
+    static let lime_light = Color("ColorLimeLight")
+    static let mango_dark = Color("ColorMangoDark")
+    static let mango_light = Color("ColorMangoLight")
+    static let pear_dark = Color("ColorPearDark")
+    static let pear_light = Color("ColorPearLight")
+    static let plum_dark = Color("ColorPlumDark")
+    static let plum_light = Color("ColorPlumLight")
+    static let pomegranate_dark = Color("ColorPomegranateDark")
+    static let pomegranate_light = Color("ColorPomegranateLight")
+    static let strawberry_dark = Color("ColorStrawberryDark")
+    static let strawberry_light = Color("ColorStrawberryLight")
+    static let watermelon_dark = Color("ColorWatermelonDark")
+    static let watermelon_light = Color("ColorWatermelonLight")
+}
---
+++ iOS-Fructus2023/View/FruitCardView.swift
@@ -0,0 +1,29 @@
+//
+//  FruitCardView.swift
+//  iOS-Fructus2023
+//
+//  Created by Gan Tu on 7/16/23.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
+    
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Blueberry")
+        } //: VSTACK
+    }
+}
+
+
+// MARK: - PREVIEW
+
+#Preview {
+    FruitCardView()
+}
